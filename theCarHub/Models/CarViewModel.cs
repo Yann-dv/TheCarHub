0f57db6d7f2ed6ee94ad8d99ea31535c5fe9a557
Yann Hagege <yhagege@d-edge.com>
2022-11-25 13:38:56 +0100

    cars controller scaffolded

---
+++ theCarHub/Models/CarViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace theCarHub.Models;
+
+public class CarViewModel
+{
+    public int CarId { get; set; }
+    public string Name { get; set; }
+    public int Year { get; set; }
+    public bool InWatchlist { get; set; }
+    public bool Watched { get; set; }
+    public int? Rating { get; set; }
+}
