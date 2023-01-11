b4c0577ef3a018a20740579c6d12c01dd0df6555
Yann Hagege <yhagege@d-edge.com>
2023-01-11 17:05:01 +0100

    connect api to controllers and add sorting car manager

--- theCarHub/Controllers/CarListController.cs
+++ theCarHub/Controllers/CarListController.cs
@@ -31,7 +31,7 @@ namespace theCarHub.Controllers
             return user?.Id;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder)
         {
             var userId = await GetCurrentUserId();
             var model = await EntityFrameworkQueryableExtensions.ToListAsync(_context.Cars.Select(x =>
@@ -61,8 +61,76 @@ namespace theCarHub.Controllers
                     item.ToSale = true;
                 }
             }
-
-            return View(model);
+            
+            ViewBag.CarIdSortParm = String.IsNullOrEmpty(sortOrder) ? "CarId_desc" : "";
+            ViewBag.YearSortParm = sortOrder == "Year" ? "Year_desc" : "Year";
+            ViewBag.BrandSortParm = sortOrder == "Brand" ? "Brand_desc" : "Brand";
+            ViewBag.ModelSortParm = sortOrder == "Model" ? "Model_desc" : "Model";
+            ViewBag.TrimSortParm = sortOrder == "Trim" ? "Trim_desc" : "Trim";
+            ViewBag.PurchasePriceSortParm = sortOrder == "PurchasePrice" ? "PurchasePrice_desc" : "PurchasePrice";
+            ViewBag.SellingPriceSortParm = sortOrder == "SellingPrice" ? "SellingPrice_desc" : "SellingPrice";
+            ViewBag.SaleDateSortParm = sortOrder == "SaleDate" ? "SaleDate_desc" : "SaleDate";
+            ViewBag.ToSaleSortParm = sortOrder == "ToSale" ? "ToSale_desc" : "ToSale";
+            
+            var modelToSort = from m in model select m;
+            switch (sortOrder)
+            {
+                case "CarId_desc":
+                    modelToSort = modelToSort.OrderByDescending(m => m.CarId);
+                    break;
+                case "Year":
+                    modelToSort = modelToSort.OrderBy(m => m.Year.Year);
+                    break;
+                case "Year_desc":
+                    modelToSort = modelToSort.OrderByDescending(m => m.Year.Year);
+                    break;
+                case "Brand":
+                    modelToSort = modelToSort.OrderBy(m => m.Brand);
+                    break;
+                case "Brand_desc":
+                    modelToSort = modelToSort.OrderByDescending(m => m.Brand);
+                    break;
+                case "Model":
+                    modelToSort = modelToSort.OrderBy(m => m.Model);
+                    break;
+                case "Model_desc":
+                    modelToSort = modelToSort.OrderByDescending(m => m.Model);
+                    break;
+                case "Trim":
+                    modelToSort = modelToSort.OrderBy(m => m.Trim);
+                    break;
+                case "Trim_desc":
+                    modelToSort = modelToSort.OrderByDescending(m => m.Trim);
+                    break;
+                case "PurchasePrice":
+                    modelToSort = modelToSort.OrderBy(m => m.PurchasePrice);
+                    break;
+                case "PurchasePrice_desc":
+                    modelToSort = modelToSort.OrderByDescending(m => m.PurchasePrice);
+                    break;
+                case "SellingPrice":
+                    modelToSort = modelToSort.OrderBy(m => m.SellingPrice);
+                    break;
+                case "SellingPrice_desc":
+                    modelToSort = modelToSort.OrderByDescending(m => m.SellingPrice);
+                    break;
+                case "SaleDate":
+                    modelToSort = modelToSort.OrderBy(m => m.SaleDate);
+                    break;
+                case "SaleDate_desc":
+                    modelToSort = modelToSort.OrderByDescending(m => m.SaleDate);
+                    break;
+                case "ToSale":
+                    modelToSort = modelToSort.OrderBy(m => m.ToSale);
+                    break;
+                case "ToSale_desc":
+                    modelToSort = modelToSort.OrderByDescending(m => m.ToSale);
+                    break;
+                default:
+                    modelToSort = modelToSort.OrderBy(m => m.CarId);
+                    break;
+                    }
+            return View(modelToSort.ToList());
         }
 
         [Authorize(Roles = "Admin")]
